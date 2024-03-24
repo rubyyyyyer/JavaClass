f4b40cb862f086e261f69602367d4aa7682a1183
Rubyyyyyer_PC <rubylin0405@.gmail.com>
2024-03-24 23:33:01 +0800

    basic bmi plus

--- app/src/main/java/com/example/javaclass/MainActivity.java
+++ app/src/main/java/com/example/javaclass/MainActivity.java
@@ -5,33 +5,38 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText inputWeight;
+    private EditText inputHeight;
+    private TextView result_bmi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
     }
-    //bmi的方法名稱，必須和onClick上的名稱一模一樣，包含大小寫。
+
+    private void findViews() {
+        inputWeight = findViewById(R.id.input_weight);
+        inputHeight = findViewById(R.id.input_height);
+        result_bmi = findViewById(R.id.result_bmi);
+    }
+
     public void bmi(View view) {
-
-        //取得輸入方塊(身高、體重)的值
-        EditText inputWeight = findViewById(R.id.input_weight);
-        EditText inputHeight = findViewById(R.id.input_height);
-
-        //將取得的值(Editable)轉換為字串(String)
         String weightS = inputWeight.getText().toString();
         String heightS = inputHeight.getText().toString();
-
-        //將身高、體重的值由字串(String)轉為浮點數(float)
         float weight = Float.parseFloat(weightS);
         float height = Float.parseFloat(heightS);
-
-        //運算bmi值
         float bmi = weight / (height*height);
-
-        //列印到控制台，類似system.out.println
         Log.d("MainActivity","BMI:" + bmi);
+        Toast.makeText(this,"Calculate BMI Result："+bmi,Toast.LENGTH_LONG).show();
+
+
+        result_bmi.setText(""+bmi);
     }
 }
